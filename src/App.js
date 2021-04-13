76e098ee4e18866f098c597b6d7172c25ef47cce
mdrzewiecki56 <mdrzewiecki56@gmail.com>
2021-04-13 21:10:27 +0200

    Enzyme testing attempt

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import "./App.less";
 import NotFound from "./components/NotFound/NotFound";
 import HeroList from "./components/HeroList/HeroList";
 import { Layout } from "antd";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
-const App = () => (
-  <Switch>
-    <Route exact path={["/", "/hero/:id?"]}>
-      <Layout className="container">
-        <Layout.Content>
-          <HeroList />
-        </Layout.Content>
-      </Layout>
-    </Route>
-    <Route component={NotFound} />
-  </Switch>
+const App = ({ store }) => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={["/", "/hero/:id?"]}>
+          <Layout className="container">
+            <Layout.Content>
+              <HeroList />
+            </Layout.Content>
+          </Layout>
+        </Route>
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
-import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
